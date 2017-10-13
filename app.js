0958fbd86f968e62adc31746b03e7ada316bf94f
Alex <Alex@Teris-MacBook-Air.local>
2017-10-13 15:33:33 -0700

    Changes to app.js. Added new-post.ejs

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ app.get("/", function(req, res){
     res.render("landingpage");
 });
 
+app.get("/new-post", function(req, res){
+	res.render("new-post");
+});
+
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log('server has started');
 });
